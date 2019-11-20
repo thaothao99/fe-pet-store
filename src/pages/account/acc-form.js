ffba55754578b87023fc9d66cc722d0e6d4b1efb
thaothao99 <17521069@gm.uit.edu.vn>
2019-11-20 15:39:50 +0700

    done update user

--- src/pages/account/acc-form.js
+++ src/pages/account/acc-form.js
@@ -1,47 +1,88 @@
 /* eslint-disable linebreak-style */
 import React from 'react'
-import { Form, Input, Button, Select, DatePicker } from 'antd'
+import { Form, Input, Button, Select, DatePicker, notification, Icon, Row, Col } from 'antd'
 import { inject, observer } from 'mobx-react'
 import { withRouter } from 'react-router-dom'
 import moment from 'moment'
+import { useMutation } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
 
 const { Option } = Select
-
+const UPDATE_USER = gql`
+  mutation updateUser($_id: String!, $input: UpdateUserInput!){
+    updateUser(_id:$_id, input:$input)
+  }
+`
 function NormalAccForm(props) {
-  const { myAcc } = props
-  console.log(myAcc)
+  const [update] = useMutation(UPDATE_USER)
+  const { myAcc, form } = props
   const hasErrors = fieldsError => Object.keys(fieldsError).some(field => fieldsError[field])
   const {
-    form: { getFieldDecorator, getFieldsError }
-  } = props
+    getFieldDecorator, getFieldsError
+  } = form
+  const handleSubmit = e => {
+    e.preventDefault()
+    form.validateFields((err, values) => {
+      if (!err) {
+        const { phone, address, birthDay, gender } = values
+        const input = {
+          phone,
+          address,
+          // eslint-disable-next-line no-underscore-dangle
+          birthDay: birthDay.format('DD/MM/YYYY'),
+          gender
+        }
+        update({
+          variables: {
+            // eslint-disable-next-line no-underscore-dangle
+            _id: myAcc._id,
+            input
+          },
+          refetchQueries: props.refetchQueries
+        })
+          .then(() => {
+            notification.open({
+              message: 'Cập nhật thành công',
+              placement: 'bottomRight',
+              icon: <Icon type="check-circle" style={{ color: '#108ee9' }} />
 
+            })
+          })
+      }
+    })
+  }
   return (
     <div className='wrapper-form-acc'>
-      <Form className="acc-form">
+      <Form onSubmit={handleSubmit} className="acc-form">
         <div className="title">
           <h2>Thông tin tài khoản</h2>
         </div>
-        <Form.Item label="Họ">
-          {getFieldDecorator('firstName', {
-            initialValue: myAcc.firstName
-          })(
-            <Input
-              size="default"
-              disabled
-            />
-          )}
-        </Form.Item>
-
-        <Form.Item label="Tên">
-          {getFieldDecorator('lastName', {
-            initialValue: myAcc.lastName
-          })(
-            <Input
-              size="default"
-              disabled
-            />
-          )}
-        </Form.Item>
+        <Row>
+          <Col span={12}>
+            <Form.Item label="Họ">
+              {getFieldDecorator('firstName', {
+                initialValue: myAcc.firstName
+              })(
+                <Input
+                  size="default"
+                  disabled
+                />
+              )}
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="Tên">
+              {getFieldDecorator('lastName', {
+                initialValue: myAcc.lastName
+              })(
+                <Input
+                  size="default"
+                  disabled
+                />
+              )}
+            </Form.Item>
+          </Col>
+        </Row>
         <Form.Item label="Email">
           {getFieldDecorator('email', {
             initialValue: myAcc.email
@@ -93,29 +134,35 @@ function NormalAccForm(props) {
             />
           )}
         </Form.Item>
-        <Form.Item label="Giới tính">
-          {getFieldDecorator('gender', {
-            initialValue: myAcc.gender
-          })(
-            <Select>
-              <Option value="Nam">Nam</Option>
-              <Option value="Nữ">Nữ</Option>
-              <Option value="Khác">Khác</Option>
-            </Select>
-          )}
-        </Form.Item>
-
-        <Form.Item label="Ngày sinh">
-          {getFieldDecorator('birthday', {
-            initialValue: !myAcc.birthDay ? moment(new Date(), 'DD/MM/YYYY') : moment(myAcc.birthday, 'DD/MM/YYYY')
-          })(
-            <DatePicker placeholder="Nhập ngày sinh" format="DD/MM/YYYY" />
-          )}
-        </Form.Item>
+        <Row>
+          <Col span={12}>
+            <Form.Item label="Giới tính">
+              {getFieldDecorator('gender', {
+                initialValue: myAcc.gender
+              })(
+                <Select>
+                  <Option value="Nam">Nam</Option>
+                  <Option value="Nữ">Nữ</Option>
+                  <Option value="Khác">Khác</Option>
+                </Select>
+              )}
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="Ngày sinh">
+              {getFieldDecorator('birthDay', {
+                initialValue: !myAcc.birthDay ? moment(new Date(), 'DD/MM/YYYY') : moment(myAcc.birthDay, 'DD/MM/YYYY')
+              })(
+                <DatePicker placeholder="Nhập ngày sinh" format="DD/MM/YYYY" />
+              )}
+            </Form.Item>
+          </Col>
+        </Row>
         <Form.Item>
           <Button
             type='default'
             className="submit-update-acc"
+            onClick={(e) => handleSubmit(e)}
             style={{ height: 46, width: '100%' }}
           >
             Cập nhật
--- src/pages/account/index.js
+++ src/pages/account/index.js
@@ -1,10 +1,10 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import React, { useState, useEffect } from 'react'
 import gql from "graphql-tag"
-import { useQuery } from '@apollo/react-hooks'
+import { useQuery, useMutation } from '@apollo/react-hooks'
+import { Button, Avatar, notification, Icon } from 'antd'
 import axios from 'axios'
 import MenuProfile from '../../components/menuProfile'
-import Layout from '../layout/index'
 import AccForm from './acc-form'
 import './index.scss'
 
@@ -24,48 +24,89 @@ const ME = gql`
   }
 }
 `
+const UPDATE_AVT = gql`
+mutation updateUrlImg($_id: String!, $urlImg: String!){
+  updateUrlImg(_id:$_id, urlImg: $urlImg)
+}
+`
 const config = {
   headers: { 'content-type': 'multipart/form-data', token: localStorage.getItem('token') }
 }
-const Account = (props) => {
-  const { history, store } = props
+const Account = () => {
   const formData = new FormData()
+  // eslint-disable-next-line no-unused-vars
+  const [updateAvt] = useMutation(UPDATE_AVT)
 
-  const { loading, data } = useQuery(ME)
-  const [img, setImg] = useState(null)
+  const { loading, data, refetch } = useQuery(ME)
+  const [img, setImg] = useState()
   const [myAcc, setMyAcc] = useState([])
+  const [imagePreviewUrl, setImagePreviewUrl] = useState()
   useEffect(() => {
     if (!loading) {
       if (data && data.me) {
         setMyAcc(data.me)
       }
     }
-  }, [data])
-  const upFile = async () => {
+  })
+  console.log(myAcc)
+  const upFile = () => {
     formData.append('image', img)
     try {
-      const response = await axios.post('http://localhost:3000', formData, config)
-      console.log(response.data)
+      axios.post('http://localhost:3000/', formData, config)
+        .then(res => {
+          updateAvt({
+            variables: {
+              // eslint-disable-next-line no-underscore-dangle
+              _id: myAcc._id,
+              urlImg: res.data.filename
+            },
+            refetchQueries: refetch
+          })
+            .then(() => {
+              setImagePreviewUrl(null)
+              setImg(null)
+              notification.open({
+                message: 'Cập nhật thành công',
+                placement: 'bottomRight',
+                icon: <Icon type="check-circle" style={{ color: '#108ee9' }} />
+              })
+            })
+        })
     } catch (err) {
       console.error(err)
     }
   }
+  const handleChange = e => {
+    const reader = new FileReader()
+    const file = e.target.files[0]
 
+    reader.onloadend = () => {
+      setImg(file)
+      setImagePreviewUrl(reader.result)
+    }
+    reader.readAsDataURL(file)
+    console.log(imagePreviewUrl)
+  }
   return (
     <div>
-      <Layout history={history} store={store} />
       <div className="acc-inf-container">
         <MenuProfile sltKey="1" class="acc-infor-left" />
-        <AccForm myAcc={myAcc} />
-        <div className="acc-inf-item-right">
-          <input type="file" name="myImage" accept="image/x-png,image/gif,image/jpeg" onChange={e => setImg(e.target.files[0])} />
-          <button onClick={() => upFile()} type="button" />
+        <AccForm myAcc={myAcc} refetchQueries={refetch} />
+        <div className="avt-form">
+          {
+            !imagePreviewUrl && myAcc.urlImg === null
+              ? (
+                <Avatar size={200} icon="user" />
+              )
+              : <Avatar size={200} src={imagePreviewUrl || myAcc.urlImg} />
+          }
+          <input type="file" name="myImage" accept="image/x-png,image/gif,image/jpeg" onChange={e => handleChange(e)} />
+          {
+            imagePreviewUrl ? <Button onClick={() => upFile()} type="default">Đặt làm ảnh đại diện</Button> : ""
+          }
         </div>
       </div>
-
-
     </div>
-
   )
 }
 export default Account
