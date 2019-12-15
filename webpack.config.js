ee4e6d153e679e70e0e9ce9194ab2efe7f86ddf7
thaothao99 <17521069@gm.uit.edu.vn>
2019-12-15 17:42:47 +0700

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 /* eslint-disable linebreak-style */
 /* eslint linebreak-style: ["error", "windows"] */
 
-const isDev = process.env.NODE_ENV === 'development'
+// const isDev = process.env.NODE_ENV === 'development'
+const isDev = true
 const { _default } = require('./config/default.config')
 
 module.exports = _default(isDev, process.env)
